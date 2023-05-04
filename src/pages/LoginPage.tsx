fe687c8d3846d19ea7780508965c7aafdc7b3482
Oleksandr Chako <oleksandr.chako@xyleme.com>
2023-05-04 15:41:33 +0200

    WIP

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -31,8 +31,19 @@ export const LoginPage: React.FC = () => {
     password: "",
   });
   const [error, setError] = useState<boolean>(false);
-
-  const [usersList, setUsersList] = useState<UserI[]>([]);
+  const [isExtended, setExtended] = useState<boolean>(false);
+  const [usersList, setUsersList] = useState<UserI[]>([
+    {
+      id: 1,
+      username: "alex",
+      password: "123",
+    },
+    {
+      id: 2,
+      username: "test_user",
+      password: "456",
+    },
+  ]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -78,7 +89,12 @@ export const LoginPage: React.FC = () => {
 
     if (isUserExist) {
       localStorage.setItem("isAuthentificated", "true");
-      navigate("/products");
+
+      if (formElements.usernameInput.value === "alex") {
+        navigate("/products");
+      }else{
+        setExtended(true)
+      }
     } else {
       localStorage.removeItem("isAuthentificated");
       setError(true);
@@ -89,6 +105,128 @@ export const LoginPage: React.FC = () => {
       });
     }
   };
+
+  const handleExtendedSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    localStorage.setItem("isAuthentificated", "true");
+    navigate("/products");
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <Stack
+        spacing={4}
+        p="1rem"
+        backgroundColor="whiteAlpha.900"
+        boxShadow="md"
+      >
+        <FormControl>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<CFaUserAlt color="gray.300" />}
+            />
+            <Input
+              isInvalid={error}
+              id="usernameInput"
+              value={credentials.username}
+              type="text"
+              placeholder="username"
+              onChange={handleUsernameChange}
+            />
+          </InputGroup>
+        </FormControl>
+        <FormControl>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              color="gray.300"
+              children={<CFaLock color="gray.300" />}
+            />
+            <Input
+              isInvalid={error}
+              id="passwordInput"
+              value={credentials.password}
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              onChange={handlePasswordChange}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                {showPassword ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button
+          borderRadius={0}
+          id="confirmLogin"
+          type="submit"
+          variant="solid"
+          colorScheme="teal"
+          width="full"
+        >
+          Login
+        </Button>
+      </Stack>
+    </form>
+  );
+
+  const extendedLoginForm = () => (
+    <form onSubmit={handleExtendedSubmit}>
+      <Stack
+        spacing={4}
+        p="1rem"
+        backgroundColor="whiteAlpha.900"
+        boxShadow="md"
+      >
+        <FormControl>
+          <InputGroup>
+            <Input
+              isInvalid={error}
+              id="phonenumberInput"
+              type="text"
+              placeholder="set your phone number"
+              onChange={handleUsernameChange}
+            />
+          </InputGroup>
+        </FormControl>
+        <FormControl>
+          <InputGroup>
+            <Input
+              isInvalid={error}
+              id="emailInput"
+              type="text"
+              placeholder="set your email"
+              onChange={handleUsernameChange}
+            />
+          </InputGroup>
+        </FormControl>
+        <FormControl>
+          <InputGroup>
+            <Input
+              isInvalid={error}
+              id="confirmEmailInput"
+              type="text"
+              placeholder="confirm your email"
+              onChange={handleUsernameChange}
+            />
+          </InputGroup>
+        </FormControl>
+        <Button
+          borderRadius={0}
+          id="confirmLogin"
+          type="submit"
+          variant="solid"
+          colorScheme="teal"
+          width="full"
+        >
+          Next
+        </Button>
+      </Stack>
+    </form>
+  );
 
   return (
     <Flex
@@ -108,62 +246,7 @@ export const LoginPage: React.FC = () => {
         <Avatar bg="teal.500" />
         <Heading color="teal.400">Welcome</Heading>
         <Box minW={{ base: "90%", md: "468px" }}>
-          <form onSubmit={handleSubmit}>
-            <Stack
-              spacing={4}
-              p="1rem"
-              backgroundColor="whiteAlpha.900"
-              boxShadow="md"
-            >
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<CFaUserAlt color="gray.300" />}
-                  />
-                  <Input
-                    isInvalid={error}
-                    id="usernameInput"
-                    value={credentials.username}
-                    type="text"
-                    placeholder="username"
-                    onChange={handleUsernameChange}
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    color="gray.300"
-                    children={<CFaLock color="gray.300" />}
-                  />
-                  <Input
-                    isInvalid={error}
-                    id="passwordInput"
-                    value={credentials.password}
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    onChange={handlePasswordChange}
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                      {showPassword ? "Hide" : "Show"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Button
-                borderRadius={0}
-                type="submit"
-                variant="solid"
-                colorScheme="teal"
-                width="full"
-              >
-                Login
-              </Button>
-            </Stack>
-          </form>
+          {isExtended ? extendedLoginForm() : loginForm()}
         </Box>
       </Stack>
     </Flex>
